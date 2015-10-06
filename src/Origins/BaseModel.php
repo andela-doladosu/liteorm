9de54711b35a6481b619e4273e4a7e6e90a4497b
user <daradosu@gmail.com>
2015-10-06 23:07:37 +0100

    Implement saving of new or updated records to database

--- src/Origins/BaseModel.php
+++ src/Origins/BaseModel.php
@@ -8,6 +8,29 @@ use Dotenv;
 abstract class BaseModel
 {
 
+    /**
+     * The name of the model 
+     * 
+     * @var string
+     */
+    protected $className;
+
+
+    /**
+     * An instance of the Connection class
+     * 
+     * @var PDO
+     */
+    protected $connection;
+
+
+    /**
+     * Array of table rows
+     * 
+     * @var array
+     */
+    protected $resultRows = [];
+
 
     public function __construct()
     {
@@ -116,6 +139,121 @@ abstract class BaseModel
     public static function find($id = null)
     {
         return self::doSearch($id);
+    }
+
+
+    /**
+     * Edit an existing row or insert a new row depending on whether the row already exists
+     * 
+     * @return bool|string
+     */
+    public function save()
+    {   
+        return $this->checkForRows() ? $this->updateRow() : $this->insertRow();
+    }
+
+
+    /**
+     * Get user assigned column values
+     * 
+     * @return array
+     */
+    protected function getAssignedValues()
+    {
+        $tableFields = $this->getTableFields();
+        $newPropertiesArray = array_slice(get_object_vars($this), 3);
+
+        $columns = $values = $tableData = [];
+
+        foreach ($newPropertiesArray as $index => $value) {
+            if (in_array($index, $tableFields)) {
+                array_push($columns, $index);
+                array_push( $values, $value);
+            }
+        }
+
+        $tableData['columns'] = $columns;
+        $tableData['values'] = $values; 
+
+        return $tableData;
+    }
+
+
+    /**
+     * Get table name for the called model
+     * 
+     * @param $model
+     * @return string
+     */
+    protected function getTableName($model)
+    {  
+        return strtolower(explode('\\', $model)[2]).'s';
+    }
+
+
+    /**
+     * Check if a row already exists
+     * 
+     * @return int
+     */
+    protected function checkForRows()
+    {
+        return count($this->resultRows);
+    }
+
+
+    /**
+     * Insert a new row 
+     * 
+     * @return string
+     */
+    protected function insertRow()
+    {   
+        $assignedValues = $this->getAssignedValues();
+
+        $columns = implode(', ', $assignedValues['columns']);
+        $values = '\''.implode('\', \'', $assignedValues['values']).'\'';
+
+        $tableName = $this->getTableName($this->className);
+        $insert = $this->connection->prepare('insert into '.$tableName.'('.$columns.') values ('.$values.')');
+
+        return $insert->execute() ? 'Row inserted successfully' : 'An error occured, unable to insert row';
+    }
+
+
+    /**
+     * Get all the column names in a table
+     * 
+     * @return array
+     */
+    protected function getTableFields()
+    {
+        $q = $this->connection->prepare('describe '.$this->getTableName($this->className));
+        $q->execute();
+        
+        return $q->fetchAll(PDO::FETCH_COLUMN);        
+    }
+
+
+    /**
+     * Edit an existing row
+     * 
+     * @return bool
+     */
+    protected function updateRow()
+    {   
+        $tableName = $this->getTableName($this->className);
+        $assignedValues = $this->getAssignedValues();
+        $updateDetails = [];
+
+        for ($i = 0; $i < count($assignedValues['columns']); $i++) { 
+            array_push($updateDetails, $assignedValues['columns'][$i]  .' =\''. $assignedValues['values'][$i].'\'');
+        }
+
+        $allUpdates = implode(', ' , $updateDetails);
+        $update = $this->connection->prepare('update '.$tableName.' set '. $allUpdates.' where id='.$this->resultRows[0]['id']);
+       
+        return $update->execute();
     }
 
 }
