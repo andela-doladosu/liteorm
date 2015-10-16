3f9a3dffd8a09c3547e2732bd21abfcd5c12ff87
user <daradosu@gmail.com>
2015-10-16 12:48:56 +0100

    Overide the loadEnv() method in the ModelStub Class to prevent loading .env file

--- tests/Stubs/ModelStub.php
+++ tests/Stubs/ModelStub.php
@@ -6,5 +6,13 @@ use Dara\Origins\BaseModel;
 
 class ModelStub extends BaseModel
 {
+    public function loadEnv()
+    {
+        $_ENV['P_DRIVER'] = '';
+        $_ENV['P_HOST']   = 'localhost';
+        $_ENV['P_DBNAME'] = 'potato';
+        $_ENV['P_USER']   = 'root';
+        $_ENV['P_PASS']   = '';
+    }
 
 }
