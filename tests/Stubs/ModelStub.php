c8a3bf70ba360264da306ae88a1b73bbea271d34
user <daradosu@gmail.com>
2015-10-16 13:30:02 +0100

    Modify ModelStub

--- tests/Stubs/ModelStub.php
+++ tests/Stubs/ModelStub.php
@@ -2,10 +2,18 @@
 
 namespace Dara\Test\Stubs;
 
+use Mockery as m;
 use Dara\Origins\BaseModel;
+use Dara\Origins\Connection;
 
 class ModelStub extends BaseModel
 {
+
+    public function __construct()
+    {
+        $this->connection = m::mock('\Dara\Origins\Connection');
+        parent::__construct();
+    }
     public function loadEnv()
     {
         $_ENV['P_DRIVER'] = 'mysql';
